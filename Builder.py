f6ee5dcb1a01b2fb731e22b9783c0db946baf342
DerrykMac <17derryk.macgregor@jc.edu.jm>
2022-09-19 17:32:27 -0500

    failed optimizations

--- Builder.py
+++ Builder.py
@@ -1,3 +1,4 @@
+from cmath import atan
 import json, csv, os, sys,math
 import Main as m
 import numpy as np
@@ -7,52 +8,83 @@ def castrayH(x, y, angle):
 
     global drr
     drr = 40
-    ##o_x = x
-    ##o_y = y
+    rx = x
+    ry = y
+    ra = angle
 
     dof = 0
-    cutoff = 500
+    cutoff = 8
     subset = 1
 
+    if angle != 0:
+        aTan = -1 / math.tan(math.radians(angle))
+    else:
+        aTan = 0
+
+    if angle > 0 and angle < 180:
+        ystep = -16
+        ry = 16 * math.floor(y / 16) + 16
+        rx = x + (ry - y) * aTan
+        xstep = ystep * aTan
+    else:
+        ystep = 16
+        ry = 16 * math.ceil(y / 16) + 0.001
+        rx =x + (ry - y) * aTan
+        xstep = ystep * aTan
+
+    if angle == 0 or angle == 180:
+        rx = x
+        ry = y
+        dof = 8
+    
+
     while dof < cutoff:
-        #move x and y by subset
-        x += ((math.cos(math.radians(angle))) / subset)
-        y += ((math.sin(math.radians(angle))) / subset)
-
-        #difference
-        ##diff_x = x - o_x
-        ##diff_y = y - o_y
-
-        #increment
         dof += 1
 
-        #check if x and y are in a wall
-        try :
-            if intmap[int(y/16)][int(x/16)] != "0":
-                (fx,fy) = (x,y)
-
-                (x,y) = (x+1,y+1)
-                if intmap[int(y/16)][int((x+2)/16)] == "0":
-                    drr = 200
-                elif intmap[int(y/16)][int((x-2)/16)] == "0":
-                    drr = 150
-                elif intmap[int((y+2)/16)][int(x/16)] == "0":
-                    drr = 100
-                elif intmap[int((y-2)/16)][int(x/16)] == "0":
-                    drr = 170
-                else:
-                    drr = 40
-
+        m_col = int(rx / 16)
+        m_row = int(ry / 16)
+        if m_col >= 0 and m_col < len(intmap[0]) and m_row >= 0 and m_row < len(intmap):
+            if intmap[m_row][m_col] != "0":
                 dof = cutoff
-                return (fx,fy)
-        except:
-            print("out of bounds")
+                drr = 255
+                print("hit")
+            else:
+                rx += xstep
+                ry += ystep
+                dof += 1
+                print("no hit")
+        else:
             dof = cutoff
-            return (x,y)
+            print("out")
     
-    if dof == cutoff:
-        drr = 255
-        return (x,y)
+    return(rx, ry)
+    
+
+
+    
+
+    
+
+    #while dof < cutoff:
+    #    dof += 1
+#
+    #    rx += ((math.cos(math.radians(angle))) * 8)
+    #    ry += ((math.sin(math.radians(angle))) * 8)
+#
+#
+    #    try :
+    #        if intmap[int(ry/16)][int(rx/16)] != "0":
+    #            
+    #            dof = cutoff
+    #    except:
+    #        print("out of bounds")
+    #        dof = cutoff
+    #
+    #if dof == cutoff:
+    #    drr = 255
+        
+        
+    return (rx, ry)
         
 
     
@@ -82,11 +114,10 @@ def Update():
             for num in row:
                 if num != " ":
                     if num == "1":
-                        m.draw.rect(m.screen, (150,150,150), m.Rect(m_row, m_col, 16, 16))
+                        m.draw.rect(m.screen, (150,150,150), m.Rect(m_row +1, m_col +1, 15, 15))
                     m_row += 16
             m_col += 16
             m_row = 0
-
     m.draw.circle(m.screen, (255,0,0), (m.Player_x, m.Player_y), 5)
 
     for i in range(-60, 60, 1):
@@ -108,9 +139,12 @@ def Update():
         floor = 499 + (hight/2)
         m.draw.rect(m.screen, (68, 219, 189), m.Rect(1000 + (i * 5), floor, 5, 1000 - floor))
         #ceiling
-        m.draw.rect(m.screen, (74, 84, 107), m.Rect(1000 + (i * 5), 0, 5, 500 - (hight/2)))
+        #m.draw.rect(m.screen, (74, 84, 107), m.Rect(1000 + (i * 5), 0, 5, 500 - (hight/2)))
         #wall
-        m.draw.rect(m.screen, col, m.Rect(1000 + (i * 5), 500 - (hight/2), 5, hight))
+        try:
+            m.draw.rect(m.screen, col, m.Rect(1000 + (i * 5), 500 - (hight/2), 5, hight))
+        except:
+            m.draw.rect(m.screen, (20, 50, 90) , m.Rect(1000 + (i * 5), 500 - (hight/2), 5, hight))
 
 
         
--- Controller.py
+++ Controller.py
@@ -5,20 +5,31 @@ import numpy as np
 def Update2():
     # move player
     # get keys down
+
+    speed = 1
+
     keys = m.key.get_pressed()
     if keys[m.K_q]:
-        m.Player_angle -= 5
+        m.Player_angle -= speed
+        if m.Player_angle < 0:
+            m.Player_angle = 360
     if keys[m.K_e]:
-        m.Player_angle += 5
+        m.Player_angle += speed
+        if m.Player_angle > 360:
+            m.Player_angle = 0
     if keys[m.K_w]:
-        m.Player_x += math.cos(math.radians(m.Player_angle)) * 5
-        m.Player_y += math.sin(math.radians(m.Player_angle)) * 5
+        m.Player_x += math.cos(math.radians(m.Player_angle)) * speed
+        m.Player_y += math.sin(math.radians(m.Player_angle)) * speed
     if keys[m.K_s]:
-        m.Player_x -= math.cos(math.radians(m.Player_angle)) * 5
-        m.Player_y -= math.sin(math.radians(m.Player_angle)) * 5
+        m.Player_x -= math.cos(math.radians(m.Player_angle)) * speed
+        m.Player_y -= math.sin(math.radians(m.Player_angle)) * speed
     if keys[m.K_a]:
-        m.Player_x -= math.cos(math.radians(m.Player_angle + 90)) * 5
-        m.Player_y -= math.sin(math.radians(m.Player_angle + 90)) * 5
+        m.Player_x -= math.cos(math.radians(m.Player_angle + 90)) * speed
+        m.Player_y -= math.sin(math.radians(m.Player_angle + 90)) * speed
     if keys[m.K_d]:
-        m.Player_x += math.cos(math.radians(m.Player_angle + 90)) * 5
-        m.Player_y += math.sin(math.radians(m.Player_angle + 90)) * 5
+        m.Player_x += math.cos(math.radians(m.Player_angle + 90)) * speed
+        m.Player_y += math.sin(math.radians(m.Player_angle + 90)) * speed
+
+    if keys[m.K_ESCAPE]:
+        m.quit()
+        m.sys.exit()
